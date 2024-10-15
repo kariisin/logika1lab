dd5b61d8602cd9d2fa3290524d14674dcf697f29
kariisin <167796501+kariisin@users.noreply.github.com>
2024-10-15 15:00:47 +0300

    Add files via upload

---
+++ lab1.1/lab1.1/lab1.1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main()
+{
+    const int size = 5;
+    int mass[size];
+
+    std::cout << "Enter  " << size << "  array elements: ";
+    for (int i = 0; i < size; ++i) {
+        std::cin >> mass[i];
+    }
+
+    int max = mass[0];
+    int min = mass[0];
+
+    for (int i = 1; i < size; ++i) {
+        if (mass[i] > max) {
+            max = mass[i];
+
+        }
+        if (mass[i] < min) {
+            min = mass[i];
+        }
+    }
+
+    int dif = max - min;
+
+    std::cout << "Deference max and min=" << dif << std::endl;
+
+    return 0;
+}
