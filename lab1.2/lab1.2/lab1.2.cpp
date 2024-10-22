e24ff2f24fbcd8bb86a295312c788bd08d435c56
kariisin <167796501+kariisin@users.noreply.github.com>
2024-10-22 10:03:43 +0300

    Add files via upload

---
+++ lab1.2/lab1.2/lab1.2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cstdlib> // для функции rand и srand
+#include <ctime>   // для функции time
+
+int main() {
+    const int size = 10; // размер массива
+    int mass[size];
+
+    // Инициализация генератора случайных чисел текущим временем
+    std::srand(static_cast<unsigned int>(std::time(0)));
+
+    // Инициализация массива случайными числами от 0 до 99
+    for (int i = 0; i < size; ++i) {
+        mass[i] = std::rand() % 100; // случайное число от 0 до 99
+    }
+
+    // Вывод массива на экран
+    for (int i = 0; i < size; ++i) {
+        std::cout << mass[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
