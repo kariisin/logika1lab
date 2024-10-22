2f722474e03af5512fbd42ea7c41c44ad605f863
kariisin <167796501+kariisin@users.noreply.github.com>
2024-10-22 10:05:20 +0300

    Add files via upload

---
+++ lab1.4/lab1.4/lab1.4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <locale.h>
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    int rows, cols;
+
+    std::cout << "Введите количество строк: ";
+    std::cin >> rows;
+    std::cout << "Введите количество столбцов: ";
+    std::cin >> cols;
+
+    // Создаем двумерный вектор (массив)
+    std::vector<std::vector<int>> array(rows, std::vector<int>(cols));
+
+    // Вводим элементы массива
+    std::cout << "Введите элементы массива:\n";
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            std::cout << "array[" << i << "][" << j << "] = ";
+            std::cin >> array[i][j];
+        }
+    }
+
+    // Вычисление сумм по столбцам
+    std::vector<int> columnSums(cols, 0);
+
+    for (int j = 0; j < cols; ++j) {
+        for (int i = 0; i < rows; ++i) {
+            columnSums[j] += array[i][j];
+        }
+    }
+
+    // Вывод сумм по столбцам
+    std::cout << "Суммы по столбцам:\n";
+    for (int j = 0; j < cols; ++j) {
+        std::cout << "Сумма столбца " << j << " = " << columnSums[j] << std::endl;
+    }
+
+    return 0;
+}
